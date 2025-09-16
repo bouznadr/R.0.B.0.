03c4776a09fdda5ba84557b1249a3947183b807f
bouznadr <rayanebouznad14@gmail.com>
2025-09-16 11:04:41 +0200

    Project commit

---
+++ Source/FileRouge/FileRougeGameModeBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "FileRougeGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class FILEROUGE_API AFileRougeGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
